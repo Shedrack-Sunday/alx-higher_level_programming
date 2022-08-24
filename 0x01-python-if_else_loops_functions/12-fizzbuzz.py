1c5687091ef5a3c42f9b48b8b0538704631a0207
Shedrack Sunday <hannania002@gmail.com>
2022-08-24 15:02:22 -0700

    FIX PY8 GOOD LINE PRACTICE

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,12 +4,12 @@ BUZZ = "Buzz"
 
 
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 and number % 5):
+    for number in range(1, 100):
+        if (number % 3 and number % 5 = 0):
             print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
+        elif (number % 3 =0):
             print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
+        elif (number % 5 = 0):
             print("%s" % (BUZZ), end=' ')
         else:
             print("%d" % (number), end=' ')
